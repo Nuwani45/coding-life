bca49fdffacddc1c5c8a0e01a4c65ac746ba6cae
Nuwani Lawanya <nuwanilawanya@hotmail.com>
2025-03-06 23:08:48 +0530

    Update Converter.java

--- Converter.java
+++ Converter.java
@@ -1,18 +1,18 @@
-// simple unit converter java code
+// Java code for a simple unit converter.
 import java.util.Scanner;
 public class Converter{
-	public static void main(String[] args){
+      public static void main(String[] args){
       Scanner scanner = new Scanner(System.in);
         
         while (true) {
-			System.out.print("Choose an option: ");
             System.out.println("Unit Converter");
+	    System.out.print("Choose an option: ");
             System.out.println("1. Meters to Kilometers");
-			System.out.println("2. Kilometers to Meters");
+	    System.out.println("2. Kilometers to Meters");
             System.out.println("3. Grams to Kilograms");
             System.out.println("4. Kilograms to Grams");
             System.out.println("5. Minutes to Hours");
-			System.out.println("6. Hours to Minutes");
+	    System.out.println("6. Hours to Minutes");
             System.out.println("7. Exit");
          
             int choice = scanner.nextInt();
@@ -43,7 +43,7 @@ public class Converter{
                     result = value * 1000;
                     System.out.println(value + " kilograms = " + result + " grams");
                     break;
-		case 5:
+		case 5: 
                     result = value / 60;
                     System.out.println(value + " minutes = " + result + " hours");
                     break;
